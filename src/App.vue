864212be64b0e7cd4a19b04e896b97f3d649e131
Fernando Vega <fjvega1@uc.cl>
2021-05-22 22:18:13 -0400

    finished chat

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <div id="app" class="row">
+<div id="app" class="container">
+  <div class="row">
     <div class="column">
-    <Map :positions="positions" :flights="flights"> </Map>
+      <Map :positions="positions" :flights="flights"> </Map>
     </div>
-    <div class="column">
-    <div id="container" v-for="flight in flights[0]" :key="flight.code">
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title">CODE: {{ flight.code }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">
-              AIRLINE: {{ flight.airline }}
-            </h6>
+  </div>
+  <div class="row">
+    
+    <div class="col-3">
+      <div class="card text-dark bg-warning mb-3" style="max-width: 18rem;">
+        <ul
+          class="list-group list-group-flush"
+          v-for="(message, id) in msgs"
+          :key="id"
+        >
+          <li class="list-group-item">
+            <span>{{ message.name }}</span
+            ><span>: {{ message.message }} </span>
+          </li>
+        </ul>
+        <form @submit.prevent="sendMessage">
+          <input v-model="username" type="text" placeholder="Your Name" />
+          <input v-model="message" type="text" placeholder="Your Message" />
+          <button>Send Message</button>
+        </form>
+      </div>
+    </div>
+    <div class="col-9">
+      <div id="container" v-for="flight in flights[0]" :key="flight.code">
+        <div class="card border-dark mb-3" style="max-width: 18rem;">
+          <div class="card-header">Code : {{ flight.code }}
+          </div>
+          <div class="card-body text-primary">
+            <h5 class="card-title">Plane : {{ flight.plane }}</h5>
             <p class="card-text">
               Plane: {{ flight.plane }}
               <br />
@@ -18,43 +40,11 @@
             </p>
           </div>
         </div>
-    </div>
-    </div>
-    <div class="column"></div>
-      <div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush" v-for="(message, id) in msgs" :key="id">
-          <li class="list-group-item"><span>{{message.name}}</span><span>: {{message.message}} </span></li>
-        </ul>
-        <form @submit.prevent="sendMessage">
-      <input v-model="username" type="text" placeholder="Your Name" />
-      <input v-model="message" type="text" placeholder="Your Message" />
-      <button>Send Message</button>
-      </form>
-        
       </div>
-    
-
-  
-  
+    </div>
   </div>
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 </template>
 
 <script>
@@ -70,43 +60,38 @@ export default {
       flights: [],
       positions: [],
       msgs: [],
-      username: '',
-      message: ''
+      username: "",
+      message: "",
     };
   },
-  
+
   sockets: {
     POSITION(data) {
-      
       this.positions.push(data.position);
-      console.log(this.positions);
-      
+     
     },
     FLIGHTS(data) {
       this.flights.push(data);
       console.log(data);
-      window.setInterval(() => {
-            this.position = [];
-        },1000);
+     
     },
-    CHAT(data){
+    CHAT(data) {
       this.msgs.push(data);
-      console.log(data)
-
-    }
-    
+     
+    },
   },
   created() {
     console.log("CREATED");
     this.$socket.client.emit("FLIGHTS");
   },
-  methods:{
-    sendMessage(){
-      this.$socket.client.emit("CHAT",{name: this.username, message: this.message});
-
-    }
-  }
- 
+  methods: {
+    sendMessage() {
+      this.$socket.client.emit("CHAT", {
+        name: this.username,
+        message: this.message,
+      });
+    },
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import VueSocketIOExt from 'vue-socket.io-extended';
 import { io } from 'socket.io-client';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from "leaflet";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 delete Icon.Default.prototype._getIconUrl;
 
